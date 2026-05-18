29a4812a34c5b508810af7cdd21edeb713e0f72e
Yelyzaveta Sh <yelyzavetash@MacBook-Air-Yelyzaveta.local>
2026-05-18 15:22:28 +0300

    Add cache key generator

--- lab3.js
+++ lab3.js
@@ -1,7 +1,13 @@
-const outdated = (item) => {
-        if (settings.expireTime === null) {
-            return false
-        }
-
-        return Date.now() > item.expires
+function createMemo(fn, config = {}) {
+    const settings = {
+        limit: config.limit ?? Infinity,
+        strategy: config.strategy ?? "LRU",
+        expireTime: config.expireTime ?? null,
+        onEvict: config.onEvict ?? null
     }
+
+    const storage = new Map()
+ const buildKey = (params) => {
+        return params.map(item => JSON.stringify(item)).join("|")
+    }
+    
