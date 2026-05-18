96f4d89f620d63debb73b04b078dcd0ddf473033
Yelyzaveta Sh <yelyzavetash@MacBook-Air-Yelyzaveta.local>
2026-05-18 14:07:26 +0300

    Initial memoization setup

--- lab3.js
+++ lab3.js
@@ -1,3 +1,7 @@
- const buildKey = (params) => {
-        return params.map(item => JSON.stringify(item)).join("|")
+const outdated = (item) => {
+        if (settings.expireTime === null) {
+            return false
+        }
+
+        return Date.now() > item.expires
     }
